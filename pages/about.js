73fa4af3ce489948bc475befc30c0dede3804af5
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-08-05 21:13:20 -0400

    ?

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/Nav'
+import Nav from '../components/nav'
 import Link from 'next/link'
 
 const About = () => (
